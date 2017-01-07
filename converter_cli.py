b219b07aef7ea4f48fd46ae746fee05e7fec8ab8
daryl <darylcoleoffice@gmail.com>
2017-01-07 15:08:03 +0900

    FIXED begins run methon

--- converter_cli.py
+++ converter_cli.py
@@ -2,7 +2,7 @@ import begin
 import converter
 
 @begin.start
-def run(image_file_path='/path/to/image.jpg', width=120, lod=127, inverted=0, custom_char='.'):
+def run(image_file_path='sample_images/isaax.jpg', width=120, lod=127, inverted=0, custom_char='.'):
     image = converter.import_image(image_file_path)
-    ascii_image = converter.do_convert(image, int(width), lod, int(inverted), custom_char, save_as)
+    ascii_image = converter.do_convert(image, int(width), lod, int(inverted), custom_char)
     print ascii_image
